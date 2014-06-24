51af48f97de1a13ce0ad8a277a3f44c36b0880bc
xianmau <xianmaulin@gmail.com>
2014-06-24 19:17:50 +0800

    update.

---
+++ src/server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", sayhelloName)       //设置访问的路由
+	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
